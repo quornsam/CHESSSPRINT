79122a563cceeadf53a661d03267d5cc8255ca4f
quornsam <120324763+quornsam@users.noreply.github.com>
2026-06-28 19:13:18 +0100

    v142 Added Leaderboard pages to both web / mobile

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "kwik-chess-pwa-v0-140";
+const CACHE_NAME = "kwik-chess-pwa-v0-142";
 const ASSETS = [
   "./",
-  "./index.html?v=0.140",
-  "./manifest.json?v=0.140",
+  "./index.html?v=0.142",
+  "./manifest.json?v=0.142",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
   "./assets/logo-king.png"
@@ -32,7 +32,7 @@ self.addEventListener("fetch", event => {
         const copy = response.clone();
         caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(() => undefined);
         return response;
-      }).catch(() => caches.match("./index.html?v=0.140"));
+      }).catch(() => caches.match("./index.html?v=0.142"));
     })
   );
 });
