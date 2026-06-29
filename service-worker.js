8a0a7b90a22049bd277075591fb86e440152f00c
quornsam <120324763+quornsam@users.noreply.github.com>
2026-06-29 23:14:32 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,31 @@
-const CACHE_NAME = "kwik-chess-pwa-v0-170";
+const CACHE_NAME = "kwik-chess-pwa-v0-171";
 const ASSETS = [
   "./",
-  "./index.html?v=0.170",
+  "./index.html?v=0.171",
   "./manifest.json",
   "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./icons/icon-512.png",
+  "./robots.txt",
+  "./sitemap.xml"
 ];
 
 self.addEventListener("install", event => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS).catch(() => undefined))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS).catch(() => undefined)));
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-    )).then(() => self.clients.claim())
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim()));
 });
 
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      if (cached) return cached;
-      return fetch(event.request).then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(() => undefined);
-        return response;
-      }).catch(() => caches.match("./index.html?v=0.170"));
-    })
-  );
+  event.respondWith(caches.match(event.request).then(cached => {
+    if (cached) return cached;
+    return fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(() => undefined);
+      return response;
+    }).catch(() => caches.match("./index.html?v=0.171"));
+  }));
 });
